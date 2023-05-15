0af2bf5e5027635d9e007df90d4e8e9c5bf5bf7b
Brett Abramson <brett.m.abramson@gmail.com>
2023-05-15 12:48:12 -0500

    reinstalled FA packages and added brand icons to social links

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,3 +1,5 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 
 const SocialLinks = () => {
@@ -5,8 +7,8 @@ const SocialLinks = () => {
     return (
         <div className="flex flex-col  h-1/3">
             <ul className="flex flex-col items-end mx-12 justify-evenly">
-                <li></li>
-                <li></li>
+                <li><FontAwesomeIcon icon={faLinkedin} /></li>
+                <li><FontAwesomeIcon icon={faGithub} /></li>
             </ul>
         </div>
     )
