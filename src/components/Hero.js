c844c063e80453718a0b816e522b36eadbc59975
Brett Abramson <brett.m.abramson@gmail.com>
2023-05-15 12:33:55 -0500

    removed FA packages, updated styling Hero and Socials

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,9 +2,9 @@ const Hero = () => {
   return (
     <div className="flex">
       <div className="flex flex-col justify-between">
-        <h1 className="text-6xl text-white font-extrabold">Brett Abramson</h1>
-        <h3 className="text-xl text-gray-500 font-serif ">Software Developer</h3>
-        <button className="bg-indigo-600 hover:bg-indigo-400 text-white font-bold py-2 px-4 rounded">About Me</button>
+        <h1 className="text-6xl text-white font-extrabold py-4">Brett Abramson</h1>
+        <h3 className="text-xl text-gray-500 font-serif py-2">Software Developer</h3>
+        <button className="bg-gradient-to-r from-indigo-900 via-indigo-700 to-indigo-500 hover:scale-110 text-white font-bold w-fit py-2 px-4 my-1 rounded">About Me</button>
       </div>
     </div>
   );
--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -5,8 +5,8 @@ const SocialLinks = () => {
     return (
         <div className="flex flex-col  h-1/3">
             <ul className="flex flex-col items-end mx-12 justify-evenly">
-                <li>linkedin</li>
-                <li>gitHub</li>
+                <li></li>
+                <li></li>
             </ul>
         </div>
     )
