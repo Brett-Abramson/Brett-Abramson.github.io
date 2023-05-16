becb48a48ab44f7e5b08a4789387948bdb567bef
Brett Abramson <brett.m.abramson@gmail.com>
2023-05-16 18:18:04 -0500

    project basic styling completed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,9 +2,15 @@ const Hero = () => {
   return (
     <div className="flex">
       <div className="flex flex-col justify-between">
-        <h1 className="text-4xl lg:text-6xl text-white font-extrabold py-4">Brett Abramson</h1>
-        <h3 className="text-xl text-gray-500 font-serif py-2">Software Developer</h3>
-        <button className="bg-gradient-to-r from-indigo-900 via-indigo-700 to-indigo-500 hover:scale-110 text-white font-bold w-fit py-2 px-4 my-1 rounded">About Me</button>
+        <h1 className="py-4 text-4xl font-extrabold text-white lg:text-6xl">
+          Brett Abramson
+        </h1>
+        <h3 className="py-2 font-serif text-xl text-gray-500">
+          Software Developer
+        </h3>
+        <button className="my-1 w-fit rounded bg-gradient-to-r from-indigo-900 via-indigo-700 to-indigo-500 px-4 py-2 font-bold text-white hover:scale-110">
+          About Me
+        </button>
       </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,43 +1,59 @@
-
-
 const Projects = () => {
+  const projects = [
+    {
+      name: "Vinyl Vault",
+      description: "An album companion and database App",
+      hostedLink:
+        "https://vinyl-vault-frontend-git-main-brettmabramson-gmailcom.vercel.app/",
+      gitHubFrontEnd: "https://github.com/Brett-Abramson/Vinyl-Vault-Frontend",
+      gitHubBackEnd: "https://github.com/Brett-Abramson/Vinyl_Vault_Backend",
+      image: "https://i.imgur.com/tpqwZFH.png",
+    },
+    {
+      name: "LANBuddy",
+      description: "A Video Game Matching and Management App",
+      hostedLink: "https://lanbuddy.herokuapp.com/",
+      gitHubFrontEnd: "https://github.com/Brett-Abramson/LANbuddy_frontend",
+      gitHubBackEnd: "https://github.com/jdbohac/project_4_backend",
+      image: "https://i.imgur.com/TPPJAtd.png",
+    },
+    {
+      name: "Lost in the Woods",
+      description: "A community sourced hiking and camping companion app",
+      hostedLink: "https://lost-in-the-woods.herokuapp.com/",
+      gitHubFrontEnd: "https://github.com/Brett-Abramson/project_3_frontend",
+      gitHubBackEnd: "https://github.com/Brett-Abramson/project_3_backend",
+      image: "https://i.imgur.com/bXzM98J.png",
+    },
+  ];
 
-    const projects = [
-        {
-            name: "Vinyl Vault",
-            description: "An album companion and database App",
-            hostedLink: "https://vinyl-vault-frontend-git-main-brettmabramson-gmailcom.vercel.app/",
-            gitHubFrontEnd: "https://github.com/Brett-Abramson/Vinyl-Vault-Frontend",
-            gitHubBackEnd: "https://github.com/Brett-Abramson/Vinyl_Vault_Backend",
-            image: "https://i.imgur.com/tpqwZFH.png"
-        },
-        {
-            name: "LANBuddy",
-            description: "A Video Game Matching and Management App",
-            hostedLink:"https://lanbuddy.herokuapp.com/",
-            gitHubFrontEnd: "https://github.com/Brett-Abramson/LANbuddy_frontend",
-            gitHubBackEnd: "https://github.com/jdbohac/project_4_backend",
-            image: "https://i.imgur.com/TPPJAtd.png"
-        },
-        {
-            name: "Lost in the Woods",
-            description: "A community sourced hiking and camping companion app",
-            hostedLink: "https://lost-in-the-woods.herokuapp.com/",
-            gitHubFrontEnd: "https://github.com/Brett-Abramson/project_3_frontend",
-            gitHubBackEnd: "https://github.com/Brett-Abramson/project_3_backend",
-            image: "https://i.imgur.com/bXzM98J.png"
-        }
-    ]
-
-    return (
-        <div className="bg-slate-100 h-full w-full">
-            <div className="flex flex-col items-center py-5">
-                <h3 className="text-2xl font-extralight pt-2">Projects</h3>
-                <h2 className="text-4xl py-6">Latest Works</h2>
-            </div>
-
+  return (
+    <div className="h-full w-full bg-slate-100">
+      <div className="flex flex-col">
+        <div className="flex flex-col items-center py-5">
+          <h3 className="pt-2 font-serif text-2xl font-extralight">Projects</h3>
+          <h2 className="py-6 text-4xl">Latest Works</h2>
         </div>
-    )
-}
+        <div className="">
+          {projects.map((project) => {
+            return (
+              <div className="flex h-[22rem] border-2 border-solid border-red-800">
+                <div className="m-auto flex h-5/6 w-5/6 bg-slate-400">
+                  <div className="flex flex-col  gap-1 justify-end px-4 pb-12">
+                    <h3 className="text-2xl font-bold">{project.name}</h3>
+                    <h5 className="font-serif font-light text-sm">{project.description}</h5>
+                    <a href={project.hostedLink}>
+                      <button className="bg-gradient-to-r from-indigo-900 via-indigo-700 to-indigo-500 px-2 py-1 my-1 font-bold text-white rounded">View Site</button>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
