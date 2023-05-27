152d733b1a4952a4a776b1098a53f482a620383a
Brett Abramson <brett.m.abramson@gmail.com>
2023-05-27 15:40:00 -0500

    mobile menu styling and responsive changes. Everything but the flair

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,25 +1,39 @@
 import MobileMenuButton from "./MobileMenuButton";
 import { useState } from "react";
+import SocialLinks from "./SocialLinks";
 
 const MobileMenu = () => {
-  const [showMobile, setShowMobile] = useState(false)
-  
+  const [showMobile, setShowMobile] = useState(false);
+
   const displayMenu = () => {
-    setShowMobile(!showMobile)
-  }
+    setShowMobile(!showMobile);
+  };
   return (
     <>
-      <MobileMenuButton displayMenu={displayMenu}/>
-      <div className={"fixed right-0 top-0 z-30 h-full overflow-x-hidden bg-emerald-950 transition duration-200 ease-out md:hidden "+ (showMobile ? "w-full" : "w-0")} >
-        <div>
-          <button onClick={()=>displayMenu()}>X</button>
+      <MobileMenuButton displayMenu={displayMenu} />
+      <div
+        className={
+          "fixed right-0 top-0 z-30 h-full w-full overflow-x-hidden bg-emerald-900 transition duration-700 ease-in-out md:hidden" +
+          (showMobile ? " translate-x-0" : " translate-x-full")
+        }
+      >
+        <div className="flex justify-end mr-4">
+          <button
+            className="mt-5  px-3 font-bold text-center text-2xl text-slate-50"
+            onClick={() => displayMenu()}
+          >
+            X
+          </button>
         </div>
-        <div className="relative top-1/4 mt-8 w-full text-center">
+        <div className="relative top-1/4 mt-6 w-full text-center">
           <ul>
-            <li>Projects</li>
-            <li>Tools</li>
-            <li>Contact</li>
+            <li className="font-bold text-4xl text-slate-50 hover:text-5xl hover:underline py-6">Projects</li>
+            <li className="font-bold text-4xl text-slate-50 hover:text-5xl hover:underline py-6">Tools</li>
+            <li className="font-bold text-4xl text-slate-50 hover:text-5xl hover:underline py-6">Contact</li>
           </ul>
+        </div>
+        <div className="absolute bottom-0">
+          <SocialLinks />
         </div>
       </div>
     </>
