c76e4f61a5da52c0dc3c8296113cf0e116603f67
Brett Abramson <brett.m.abramson@gmail.com>
2023-05-27 13:15:28 -0500

    moved MobileMenuButton inside Mobile Menu and added toggle functionality using State

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,17 +1,28 @@
+import MobileMenuButton from "./MobileMenuButton";
+import { useState } from "react";
+
 const MobileMenu = () => {
+  const [showMobile, setShowMobile] = useState(false)
+  
+  const displayMenu = () => {
+    setShowMobile(!showMobile)
+  }
   return (
-    <div className="fixed left-0 top-0 z-30 h-full w-0 overflow-x-hidden bg-emerald-950 transition duration-200 ease-in md:hidden">
-      <div>
-        <button>X</button>
+    <>
+      <MobileMenuButton displayMenu={displayMenu}/>
+      <div className={"fixed right-0 top-0 z-30 h-full overflow-x-hidden bg-emerald-950 transition duration-200 ease-out md:hidden "+ (showMobile ? "w-full" : "w-0")} >
+        <div>
+          <button onClick={()=>displayMenu()}>X</button>
+        </div>
+        <div className="relative top-1/4 mt-8 w-full text-center">
+          <ul>
+            <li>Projects</li>
+            <li>Tools</li>
+            <li>Contact</li>
+          </ul>
+        </div>
       </div>
-      <div className="relative top-1/4 mt-8 w-full text-center">
-        <ul>
-          <li>Projects</li>
-          <li>Tools</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-    </div>
+    </>
   );
 };
 
