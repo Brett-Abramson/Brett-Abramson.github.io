c7513cd879240de28b7255da037a7cfaa60d5f51
Brett Abramson <brett.m.abramson@gmail.com>
2023-05-27 21:58:01 -0500

    commit branch before checkingout -> made button slightly bigger on mobile hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
         <h3 className="pb-4 font-serif text-xl text-zinc-300 lg:text-2xl xl:text-3xl">
           Software Developer
         </h3>
-        <button className=" w-fit rounded bg-gradient-to-r from-indigo-900 via-indigo-700 to-indigo-500 px-4 py-2 text-sm font-semibold text-slate-50 hover:scale-110 md:text-base xl:text-xl">
+        <button className="w-fit rounded bg-gradient-to-r from-indigo-900 via-indigo-700 to-indigo-500 px-4 py-2  font-semibold text-slate-50 hover:scale-110 md:text-base xl:text-xl">
           About Me
         </button>
       </div>
