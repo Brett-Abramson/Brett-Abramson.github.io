dc5bf53d9f4fca06574ffac5fd8f9d01f051d86d
Brett Abramson <brett.m.abramson@gmail.com>
2023-05-30 11:07:03 -0500

    responsive project styling complete for now

---
+++ src/components/ProjectModal.js
@@ -0,0 +1,12 @@
+const ProjectModal = (props) => {
+
+    return (
+        <div className="hidden fixed z-10 left-0 top-0 w-full h-full overflow-auto">
+            <div className="mt-[15%] mx-auto p-5 border w-4/5">
+                
+            </div>
+        </div>
+    )
+}
+
+export default ProjectModal
