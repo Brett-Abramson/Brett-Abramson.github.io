331379e503f03d2a12450b0f49503fc8305cb6f9
Brett Abramson <brett.m.abramson@gmail.com>
2023-06-05 10:54:33 -0500

    added links to aboutMeModal, some more styling, still WIP

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -17,7 +17,43 @@ const AboutModal = (props) => {
         <div className="flex w-full flex-col justify-center rounded border-x border-y border-solid border-zinc-50 bg-zinc-800 text-slate-50 sm:m-auto sm:max-w-md md:max-w-lg lg:max-w-xl">
           <div className="mx-6">
             <h2 className="m-4 mt-8 text-2xl">About Me</h2>
-            <p className=""></p>
+            <p className="pb-4 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              vulputate varius sodales. Nullam aliquam ex vitae purus rutrum
+              dapibus. Ut arcu mi, rhoncus ut risus id, lobortis finibus risus.
+              Donec nec felis in enim egestas pulvinar. Pellentesque porttitor
+              cursus porttitor. Mauris tempor erat quis mauris dictum, sed
+              placerat lacus posuere. Donec id fringilla leo. Maecenas
+              scelerisque rutrum sagittis. Maecenas sodales felis eget ipsum
+              maximus pellentesque. Nunc velit eros, feugiat in feugiat eget,
+              interdum at tellus. Mauris ac nunc ac felis vestibulum interdum.
+            </p>
+            <div>
+                <h4 className="text-right my-2">Want to learn more?</h4>
+              <ul className="my-4 flex justify-around">
+                <li className="hover:text-white hover:scale-105 hover:underline">
+                  <a
+                    href="https://www.linkedin.com/in/brettmabramson/"
+                    target="_blank"
+                    className="rounded border bg-slate-600 p-1 text-lg "
+                  >
+                    LinkedIN
+                  </a>
+                </li>
+                <li className="hover:text-white hover:scale-105 hover:underline">
+                  <a
+                    href="https://github.com/Brett-Abramson"
+                    target="_blank"
+                    className="rounded border bg-slate-600 p-1 text-lg"
+                  >
+                    github
+                  </a>
+                </li>
+                <li className="hover:text-white hover:scale-105 hover:underline">
+                    <a href="https://docs.google.com/document/d/1mIJZNi9hhq4iV6RIKMfDjweFYtqGjTKJPsDaFCwXAkQ/edit#heading=h.gjdgxs" target="_blank" className="rounded border bg-slate-600 p-1 text-lg">Resume</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const SocialLinks = () => {
   return (
     <div className="flex justify-center w-screen md:w-full  md:flex md:justify-end">
-      <ul className="flex justify-around w-full px-10 py-10 text-4xl text-slate-50 md:mx-10 md:flex-col md:text-zinc-300 md:w-0 lg:text-3xl">
+      <ul className="flex justify-around w-full px-10 py-10 text-4xl text-slate-50 md:mx-10 md:flex-col md:text-zinc-300 md:w-0 slg:text-3xl">
         <li className="my-4 md:mx-0">
           <a href="https://www.linkedin.com/in/brettmabramson/" target="_blank">
             <FontAwesomeIcon icon={faLinkedin} className="hover:scale-110 hover:text-white"/>
