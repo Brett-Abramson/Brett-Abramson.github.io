3488f270bd5a8fde1ef0afda66ad81400444aaee
Brett Abramson <brett.m.abramson@gmail.com>
2023-06-08 11:21:24 -0700

    added e-mail and copyright to footer. Mobile styling is nearly complete, polishing, need to test bigger screen sizes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Footer = () => {
   return (
     <footer className="pt-8">
-      <div className="m-auto flex h-full w-screen flex-wrap bg-slate-950">
+      <div className="m-auto flex h-full w-screen flex-wrap bg-slate-950 text-slate-50">
         <div className="flex  w-full flex-col items-center justify-center py-4">
-          <div className="flex my-2 text-lg flex-col text-slate-50">
-            <FontAwesomeIcon icon={faArrowUp} className="text-2xl md:animate-bounce"/>
-            <h4 className="hidden md:block">Back To Top</h4>
-          </div>
-          <ul className="flex w-1/2 justify-around text-3xl text-slate-50">
-            <li className="my-4 md:mx-0">
+          <a href="#__next">
+            <div className="my-2 flex flex-col text-lg">
+              <FontAwesomeIcon
+                icon={faArrowUp}
+                className="text-2xl md:animate-bounce"
+              />
+              <h4 className="hidden md:block">Back To Top</h4>
+            </div>
+          </a>
+          <ul className="flex w-1/2 justify-around text-2xl">
+            <li className="my-3 md:mx-0">
               <a
                 href="https://www.linkedin.com/in/brettmabramson/"
                 target="_blank"
@@ -23,7 +28,7 @@ const Footer = () => {
                 />
               </a>
             </li>
-            <li className="my-4 md:mx-0">
+            <li className="my-3 md:mx-0">
               <a href="https://github.com/Brett-Abramson" target="_blank">
                 <FontAwesomeIcon
                   icon={faGithub}
@@ -32,6 +37,16 @@ const Footer = () => {
               </a>
             </li>
           </ul>
+          <a
+            href="mailto:brett.m.abramson@gmail.com"
+            className="my-1 text-sm text-slate-200"
+          >
+            brett.m.abramson@gmail.com
+          </a>
+          <p className="my-1 text-sm text-slate-300">
+            &copy; {new Date().getFullYear()} Brett Abramson. All rights
+            reserved.
+          </p>
         </div>
       </div>
     </footer>
