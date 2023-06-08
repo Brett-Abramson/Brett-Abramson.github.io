cbd1e961403ae8fa8cef4a45d2282a49675ad3b4
Brett Abramson <brett.m.abramson@gmail.com>
2023-06-08 10:09:27 -0700

    improved styling of skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -35,8 +35,8 @@ const Skills = () => {
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
     },
     {
-        name: "Bootstrap",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg"
+      name: "Bootstrap",
+      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg",
     },
     {
       name: "Django",
@@ -61,14 +61,19 @@ const Skills = () => {
   ];
 
   return (
-    <div className="text-slate-100" id="skills">
-      <h2 className="my-8 text-center text-3xl font-bold md:my-14">Skills</h2>
-      <div className="mx-8 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+    <div
+      className="rounded text-slate-100 md:container md:my-12 md:min-h-screen md:bg-zinc-800 md:py-8 lg:pb-20"
+      id="skills"
+    >
+      <h2 className="my-8 text-center text-3xl font-bold md:my-10 md:text-4xl lg:my-12">
+        Skills
+      </h2>
+      <div className="mx-8 grid grid-cols-2 gap-4 sm:grid-cols-3 md:mx-12 md:grid-cols-4 md:gap-5 lg:mx-16 lg:grid-cols-5 lg:gap-6 xl:gap-8">
         {technologies.map((technology, index) => {
           return (
             <div
               key={index}
-              className="flex flex-col items-center justify-center rounded-lg bg-slate-100 p-4 text-slate-950 shadow-md"
+              className="flex flex-col items-center justify-center rounded-lg bg-slate-100 p-4 text-slate-950 shadow-md xl:py-7"
               title={technology.name}
             >
               <Image
@@ -76,8 +81,9 @@ const Skills = () => {
                 height={32}
                 src={technology.icon}
                 alt={`${technology.name} Icon`}
+                className="lg:w-10 xl:w-12 "
               />
-              <h5 className="font-serif">{technology.name}</h5>
+              <h5 className="mt-2 font-serif xl:text-lg">{technology.name}</h5>
             </div>
           );
         })}
