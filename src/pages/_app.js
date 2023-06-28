e137f40dc0814c984355da84d12f54eb6ca0c1fa
Brett Abramson <brett.m.abramson@gmail.com>
2023-06-28 14:35:02 -0700

    logging pageviews in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,26 @@
 import Layout from "@/context/Layout";
 import "@/styles/globals.css";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
+import * as ga from "../lib/ga"
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
 
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    // When the component is mounted, subscribe to router changes and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange)
 
+    // If the component is unmounted, unsubsrive from the event with the "off" method
+    return () => {
+      router.events.off("routerChangeComplete", handleRouteChange)
+    }
+  } [router.events])
   return (
     <>
       <Head>
