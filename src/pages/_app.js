9c6779935b5ff390579a845aa0b216fbda48c74a
Brett Abramson <brett.m.abramson@gmail.com>
2023-06-29 14:36:41 -0700

    console.log(env) for GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,8 @@ import { useEffect } from "react";
 import * as ga from "../lib/ga"
 
 export default function App({ Component, pageProps }) {
+  console.log(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS)
+  
   const router = useRouter()
 
   useEffect(() => {
