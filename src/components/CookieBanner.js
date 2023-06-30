e617ba1820ba86f3a372fa95b9f443b9862e917e
Brett Abramson <brett.m.abramson@gmail.com>
2023-06-30 10:43:22 -0700

    updated styling of cookie banner

--- src/components/CookieBanner.js
+++ src/components/CookieBanner.js
@@ -31,23 +31,23 @@ export default function CookieBanner() {
     }, [cookieConsent])
   return (
     <div
-      className={`fixed bottom-0 left-0 right-0 mx-auto my-10 ${cookieConsent != null ? "hidden" : "flex"} max-w-max flex-col items-center justify-between gap-4 rounded-lg bg-gray-700 px-3 py-3 shadow sm:flex-row md:max-w-screen-sm md:px-4`}
+      className={`fixed bottom-0 left-0 right-0 mx-auto my-10 ${cookieConsent != null ? "flex" : "flex"} max-w-max flex-col items-center justify-between gap-4 rounded-lg bg-slate-800 px-3 py-3 shadow sm:flex-row md:max-w-screen-sm md:px-4`}
     >
-      <div className="text-center">
+      <div className="text-center text-slate-50">
         <Link href="/info/cookies">
           <p>
-            We use <span className="font-bold text-sky-400">cookies</span> on
-            our site.
+            There's a <span className="font-bold text-sky-400">cookie</span> on
+            this site.
           </p>
         </Link>
       </div>
 
       <div className="flex gap-2">
-        <button className="rounded-md border-gray-900 px-5 py-2 text-gray-300" onClick={() => setCookieConsent(false)}>
+        <button className="rounded-md bg-gray-900 px-5 py-2 text-gray-300" onClick={() => setCookieConsent(false)}>
           Decline
         </button>
         <button className="rounded-lg bg-gray-900 px-5 py-2 text-white" onClick={() => setCookieConsent(true)}>
-          Allow Cookies
+          Allow Cookie
         </button>
       </div>
     </div>
