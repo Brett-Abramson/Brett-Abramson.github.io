af040556989a928d4ecbf0c84880686c2123d34a
Brett Abramson <brett.m.abramson@gmail.com>
2023-06-30 08:19:16 -0700

    using GA script in my _document file and Context/Layout.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,29 +1,9 @@
 import Layout from "@/context/Layout";
 import "@/styles/globals.css";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-
-import * as ga from "../lib/ga"
 
 export default function App({ Component, pageProps }) {
-  console.log(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS)
-  
-  const router = useRouter()
 
-  useEffect(() => {
-    ga.initGA()
-    ga.logPageView()
-
-    const handleRouteChange = () => {
-      ga.logPageView()
-    }
-    router.events.on("routeChangeComplete", handleRouteChange)
-
-    return () => {
-      router.events.off("routerChangeComplete", handleRouteChange)
-    }
-  }, [router.events])
 
   return (
     <>
