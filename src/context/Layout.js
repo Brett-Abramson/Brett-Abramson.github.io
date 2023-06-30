6f8a056999b87a0c4cb640b52493cf3b9dd461b6
Brett Abramson <brett.m.abramson@gmail.com>
2023-06-30 08:19:16 -0700

    using GA script in my _document file and Context/Layout.js

--- src/context/Layout.js
+++ src/context/Layout.js
@@ -3,7 +3,7 @@ import GoogleAnalytics from "@/lib/ga";
 const Layout = ({ children }) => {
   return (
     <>
-      <GoogleAnalytics GA_MEASUREMENT_ID={G-NTHSZKDJEP} />
+      <GoogleAnalytics GA_MEASUREMENT_ID="G-NTHSZKDJEP" />
       <div className="h-full min-h-screen bg-zinc-900 py-10">
         <div>{children}</div>
       </div>
