edc45731ecf9a696f54e2f38c23b7057e8aa9817
Brett Abramson <brett.m.abramson@gmail.com>
2023-06-30 09:36:31 -0700

    updating next.config.js for gitHub build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: "export",
-  // basePath: "/Brett-Abramson.github.io",
+  output: "export",
+  basePath: "/Brett-Abramson.github.io",
 }
 
 module.exports = nextConfig
