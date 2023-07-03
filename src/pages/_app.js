734decf5adaf275642b249724197b8142f57df77
Brett Abramson <brett.m.abramson@gmail.com>
2023-07-03 15:59:17 -0700

    moved Script again

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from "@/context/Layout";
 import "@/styles/globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -12,6 +13,24 @@ export default function App({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        id="google-analytics-inline"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag("js", new Date());
+            gtag("config", "${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}", {
+              page_path: window.location.pathname,
+            });
+            `,
+        }}
+      />
     </>
   );
 }
