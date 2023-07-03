c6117c29452ea74a15a6cfc746b974e79f312d1b
Brett Abramson <brett.m.abramson@gmail.com>
2023-07-03 14:41:30 -0700

    importing Script correctly

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from "@/context/Layout";
 import "@/styles/globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
