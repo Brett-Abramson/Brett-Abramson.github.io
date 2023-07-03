81684df796f808741d09cedc50e96b3c8082b3cb
Brett Abramson <brett.m.abramson@gmail.com>
2023-07-03 14:40:35 -0700

    moved Script from _doc to _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,15 +3,31 @@ import "@/styles/globals.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-
-
   return (
     <>
       <Head>
         <title>Brett Abramson</title>
       </Head>
+
       <Layout>
         <Component {...pageProps} />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag("js", new Date());
+            gtag("config", "${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}", {
+              page_path: window.location.pathname,
+            });
+            `,
+          }}
+        />
       </Layout>
     </>
   );
