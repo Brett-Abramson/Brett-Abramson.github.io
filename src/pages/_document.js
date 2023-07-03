847d5b6d7b01a81fc6105de6a48cc40b7b5a0ec2
Brett Abramson <brett.m.abramson@gmail.com>
2023-07-03 14:05:45 -0700

    updating GA code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+
+  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
   return (
     <Html lang="en">
       <Head>
         {/* Global Site Tag (gtag.js) - Google Analytics */}
         <script
           async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         />
         <script
           dangerouslySetInnerHTML={{
@@ -15,7 +17,7 @@ export default function Document() {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag("js", new Date());
-            gtag("config", "${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}", {
+            gtag("config", "${GA_MEASUREMENT_ID}", {
               page_path: window.location.pathname,
             });
             `,
