d46bf5383a5bfc9cefa0c12a98775ed095c47509
Brett Abramson <brett.m.abramson@gmail.com>
2023-07-03 15:50:26 -0700

    moved Script back to _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,14 +1,28 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Script from "next/script";
 
 export default function Document() {
-
-
   return (
     <Html lang="en">
-      <Head>
-
-      </Head>
+      <Head></Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        id="google-analytics-inline"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag("js", new Date());
+            gtag("config", "${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}", {
+              page_path: window.location.pathname,
+            });
+            `,
+        }}
+      />
       <body>
         <Main />
         <NextScript />
