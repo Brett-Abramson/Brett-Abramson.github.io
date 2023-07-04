a03433bf1c7ce9067975296f12edaa243580e700
Brett Abramson <brett.m.abramson@gmail.com>
2023-07-03 17:53:49 -0700

    added url 's to projects and increased size of text in header on md:

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,25 @@ const Header = (props) => {
     <nav className="min-w-full">
       <div>
         {/* <DropDown /> */}
-        <MobileMenu toggleModal={props.toggleModal}/>
+        <MobileMenu toggleModal={props.toggleModal} />
       </div>
       <div className="mr-6 flex justify-end">
         <div className="hidden md:flex">
           <a
             href="#projects"
-            className="mx-4 text-zinc-300 hover:text-white hover:underline lg:mx-6 lg:text-3xl"
+            className="mx-4 text-zinc-300 hover:text-white hover:underline md:text-xl lg:mx-6 lg:text-3xl"
           >
             Projects
           </a>
           <a
             href="#skills"
-            className="mx-4 text-zinc-300 hover:text-white hover:underline lg:mx-6 lg:text-3xl"
+            className="mx-4 text-zinc-300 hover:text-white hover:underline md:text-xl lg:mx-6 lg:text-3xl"
           >
             Skills
           </a>
           <a
             href="#aboutMe"
-            className="mx-4 text-zinc-300 hover:text-white hover:underline lg:mx-6 lg:text-3xl"
+            className="mx-4 text-zinc-300 hover:text-white hover:underline md:text-xl lg:mx-6 lg:text-3xl"
             onClick={props.toggleModal}
           >
             Contact
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,62 +1,77 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Skills = () => {
   const technologies = [
     {
       name: "Javascript",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     },
     {
       name: "Python",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+      url: "https://www.python.org",
     },
     {
       name: "HTML5",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+      url: "https://www.w3.org/html/",
     },
     {
       name: "CSS",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+      url: "https://www.w3schools.com/css/",
     },
     {
       name: "React",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+      url: "https://reactjs.org/",
     },
     {
       name: "Next.js",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+      url: "https://nextjs.org/",
     },
     {
       name: "TailwindCSS",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+      url: "https://tailwindcss.com/",
     },
     {
       name: "MaterialUI",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+      url: "https://mui.com/",
     },
     {
       name: "Bootstrap",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg",
+      url: "https://getbootstrap.com",
     },
     {
       name: "Django",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg",
+      url: "https://www.djangoproject.com/",
     },
     {
       name: "MongoDB",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg",
+      url: "https://www.mongodb.com/",
     },
     {
       name: "PostGreSQL",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+      url: "https://www.postgresql.org",
     },
     {
       name: "Express",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+      url: "https://expressjs.com",
     },
     {
       name: "Node.js",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+      url: "https://nodejs.org",
     },
   ];
 
@@ -71,20 +86,24 @@ const Skills = () => {
       <div className="mx-8 grid grid-cols-2 gap-4 sm:grid-cols-3 md:mx-12 md:grid-cols-4 md:gap-5 lg:mx-16 lg:grid-cols-5 lg:gap-6 xl:gap-8">
         {technologies.map((technology, index) => {
           return (
-            <div
-              key={index}
-              className="flex flex-col items-center justify-center rounded-lg bg-slate-100 p-4 text-slate-950 shadow-md xl:py-7"
-              title={technology.name}
-            >
-              <Image
-                width={32}
-                height={32}
-                src={technology.icon}
-                alt={`${technology.name} Icon`}
-                className="lg:w-10 xl:w-12 "
-              />
-              <h5 className="mt-2 font-serif xl:text-lg">{technology.name}</h5>
-            </div>
+            <Link href={technology.url} target="_blank" rel="noreferrer">
+              <div
+                key={index}
+                className="flex flex-col items-center justify-center rounded-lg bg-slate-100 p-4 text-slate-950 shadow-md xl:py-7"
+                title={technology.name}
+              >
+                <Image
+                  width={32}
+                  height={32}
+                  src={technology.icon}
+                  alt={`${technology.name} Icon`}
+                  className="lg:w-10 xl:w-12 "
+                />
+                <h5 className="mt-2 font-serif xl:text-lg">
+                  {technology.name}
+                </h5>
+              </div>
+            </Link>
           );
         })}
       </div>
