b40858bbce014d24ac4defcaa461152215dbd437
Brett Abramson <brett.m.abramson@gmail.com>
2023-09-22 16:59:46 -0500

    removed bottom scroll bar

--- src/context/Layout.js
+++ src/context/Layout.js
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <GoogleAnalytics />
-      <div className="h-full min-h-screen bg-zinc-900 py-10">
+      <div className="h-full min-h-screen overflow-auto overflow-x-hidden bg-zinc-900 py-10">
         <div>{children}</div>
         <CookieBanner />
       </div>
