2085cb62f2c6f64b289406db9b0d673430055f18
Brett Abramson <brett.m.abramson@gmail.com>
2023-09-25 16:21:46 -0500

    replaced .gifs with .pm4 increasing quality. Still need to test performance

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // Exclude MP4 files from being processed by webpack
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/", // Specify the path where Next.js should serve the MP4 files from
+          outputPath: "static/",
+        },
+      },
+    });
+
+    return config;
+  },
   // output: "export",
   // basePath: "/Brett-Abramson.github.io",
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
