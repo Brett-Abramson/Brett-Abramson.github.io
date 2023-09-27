b179a149a552aebb9aaf9fd01cfbcb53b2f1d028
Brett Abramson <brett.m.abramson@gmail.com>
2023-09-27 14:53:16 -0500

    short story text size increased

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -23,18 +23,19 @@ const AboutModal = (props) => {
             <h2 className="mt-8 text-2xl">About Me</h2>
             <div className="mb-4 w-2/3 bg-indigo-700 py-1 sm:w-1/2" />
             <h4 className="font-bold">Short Story:</h4>
-            <p className="mx-4 pb-4 indent-4 text-sm">
+            <p className="mx-4 pb-4 indent-4">
               I am a software developer based in Austin, Texas driven by the art
               of crafting code that harmonizes seamlessly with project goals. As
               well as a coder, I am also an avid brewer and musician. What truly
               fascinates me is the common thread running through these passions.
-              Whether it&apos;s evoking emotions through music, creating delectable
-              libations through brewing, or designing intuitive and efficient
-              applications, they all thrive on distinct and intricate processes,
-              each demanding nuanced approaches to acheive excellence.
+              Whether it&apos;s evoking emotions through music, creating
+              delectable libations through brewing, or designing intuitive and
+              efficient applications, they all thrive on distinct and intricate
+              processes, each demanding nuanced approaches to acheive
+              excellence.
             </p>
             <details>
-              <summary className="font-bold cursor-pointer">Long Story</summary>
+              <summary className="cursor-pointer font-bold">Long Story</summary>
               <p className="mx-4 pb-4 pt-4 indent-6 text-sm">
                 Hello! Thank you for taking the time to get to know me better. I
                 grew up in the San Francisco Bay Area before an unexpected twist
@@ -44,32 +45,32 @@ const AboutModal = (props) => {
                 mountains, and the proximity to the ocean. However, life had
                 other plans; car troubles and financial constraints meant I was
                 grounded in Austin for a while. As time went on, my North
-                Carolina dream faded, and I found happiness in Austin&apos;s vibrant
-                atmosphere.
+                Carolina dream faded, and I found happiness in Austin&apos;s
+                vibrant atmosphere.
               </p>
               <p className="mx-4 pb-4 text-sm">
                 Before embarking on this journey, I gained valuable experience
-                working in a family restaurant on Fisherman&apos;s Wharf. There, I
-                played a pivotal role in modernizing the computer systems by
+                working in a family restaurant on Fisherman&apos;s Wharf. There,
+                I played a pivotal role in modernizing the computer systems by
                 installing and configuring new POS software. My passion for
                 tinkering with computer systems was a constant in my life, and
                 even during my earlier years, I was the go-to tech expert for
                 friends and family. After completing my degree in Liberal
                 Studies with a focus on Multi-disciplinary Ethnic Studies at San
                 Francisco State University, I took some time to explore
-                different career paths. For the past eight years, I&apos;ve been a
-                bartender at events and weddings in Austin, which allowed me to
-                pursue my other interests, such as playing in a band and brewing
-                beer. During the COVID pandemic, my enthusiasm for computers and
-                coding reignited. After several years of self-study and
-                consulting with friends already working as developers, I made
-                the decision to take the plunge and enroll in a coding boot
-                camp.
+                different career paths. For the past eight years, I&apos;ve been
+                a bartender at events and weddings in Austin, which allowed me
+                to pursue my other interests, such as playing in a band and
+                brewing beer. During the COVID pandemic, my enthusiasm for
+                computers and coding reignited. After several years of
+                self-study and consulting with friends already working as
+                developers, I made the decision to take the plunge and enroll in
+                a coding boot camp.
                 <br />
                 <br />
-                When I&apos;m not engrossed in keyboard activities, you&apos;ll likely
-                find me immersed in music, crafting homemade brews, or embracing
-                outdoor adventures.
+                When I&apos;m not engrossed in keyboard activities, you&apos;ll
+                likely find me immersed in music, crafting homemade brews, or
+                embracing outdoor adventures.
               </p>
             </details>
 
@@ -77,7 +78,7 @@ const AboutModal = (props) => {
               <h4 className="my-2 mr-8 animate-pulse text-right md:text-lg">
                 Connect!
               </h4>
-              <ul className="my-4 lg:my-8 flex justify-between lg:justify-around">
+              <ul className="my-4 flex justify-between lg:my-8 lg:justify-around">
                 <li className="hover:scale-105 hover:text-white hover:underline">
                   <Link
                     href="https://www.linkedin.com/in/brettmabramson/"
