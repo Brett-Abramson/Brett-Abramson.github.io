257b6aacb59ed00bdc2a06c0a6a178b3d0f09490
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-01 11:34:09 -0500

    added a try-catch block to sendEmail api

--- src/pages/api/sendEmail.js
+++ src/pages/api/sendEmail.js
@@ -4,37 +4,39 @@ export default async (req, res) => {
   const myEmail = process.env.MY_EMAIL;
   const portfolioEmail = process.env.PORTFOLIO_EMAIL;
   const portfolioPassword = process.env.PORTFOLIO_PASSWORD;
+  try {
+    if (req.method === "POST") {
+      const { name, email, message } = req.body;
 
-  if (req.method === "POST") {
-    const { name, email, message } = req.body;
+      // A Transporter using my email service credentials
+      const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+          user: portfolioEmail,
+          pass: portfolioPassword,
+        },
+      });
 
-    // A Transporter using my email service credentials
-    const transporter = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-        user: portfolioEmail, 
-        pass: portfolioPassword,
-      },
-    });
+      // set up email data
+      const mailOptions = {
+        from: email,
+        to: myEmail,
+        subject: `New Contact from ${name}`,
+        text: message,
+      };
 
-    // set up email data
-    const mailOptions = {
-      from: email,
-      to: myEmail,
-      subject: `New Contact from ${name}`,
-      text: message,
-    };
-
-    // send email
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        res.status(500).json({ message: "error sending email." });
-      } else {
-        res.status(200).json({ message: "Email sent! " });
-      }
-    });
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
+      // send email
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(JSON.stringify(error));
+          res.status(500).json({ message: "error sending email." });
+        } else {
+          res.status(200).json({ message: "Email sent! " });
+        }
+      });
+    }
+  } catch (exception) {
+    console.log("Exception caught:", JSON.stringify(exception));
+    return res.status(500).json({ message: "internal server error." });
   }
 };
