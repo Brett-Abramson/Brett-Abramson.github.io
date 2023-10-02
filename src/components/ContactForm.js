c1e12d0a4cbb5acc0e072703190781d15dfcf982
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-02 11:32:21 -0500

    adding autocomplete to contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -93,6 +93,7 @@ const ContactForm = () => {
                     name="name"
                     value={formData.name}
                     required
+                    autoComplete="name"
                     className="rounded text-zinc-950"
                     onChange={handleChange}
                   />
@@ -104,6 +105,7 @@ const ContactForm = () => {
                     name="email"
                     value={formData.email}
                     required
+                    autoComplete="email"
                     className="rounded text-zinc-950"
                     onChange={handleChange}
                   />
