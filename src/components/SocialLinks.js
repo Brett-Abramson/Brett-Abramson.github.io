613aa14610ebc1f0ea6c92c57fd9bcc23237a0f0
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-09 15:14:45 -0700

    added missing aria-labels to links

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -8,12 +8,12 @@ const SocialLinks = () => {
     <div className="flex justify-center w-screen md:w-full  md:flex md:justify-end">
       <ul className="flex justify-around w-full px-10 py-10 text-4xl text-slate-50 md:mx-10 md:flex-col md:text-zinc-300 md:w-0">
         <li className="my-4 md:mx-0">
-          <Link href="https://www.linkedin.com/in/brettmabramson/" target="_blank">
+          <Link href="https://www.linkedin.com/in/brettmabramson/" target="_blank" aria-label="Visit Brett's linkedIn">
             <FontAwesomeIcon icon={faLinkedin} className="hover:scale-110 hover:text-white"/>
           </Link>
         </li>
         <li className="my-4 md:mx-0">
-          <Link href="https://github.com/Brett-Abramson" target="_blank">
+          <Link href="https://github.com/Brett-Abramson" target="_blank" aria-label="Visit Brett's github">
             <FontAwesomeIcon icon={faGithub} className="hover:scale-110 hover:text-white"/>
           </Link>
         </li>
