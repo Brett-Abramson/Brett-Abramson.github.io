c9dc0564a3d8a9d389bde8ac84ebf9918af08224
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-09 14:39:59 -0700

    removed google analytics and cookie banner

--- src/context/Layout.js
+++ src/context/Layout.js
@@ -1,13 +1,8 @@
-import CookieBanner from "@/components/CookieBanner";
-import GoogleAnalytics from "@/lib/ga";
-
 const Layout = ({ children }) => {
   return (
     <>
-      <GoogleAnalytics />
       <div className="h-full min-h-screen overflow-auto overflow-x-hidden bg-zinc-900 py-10">
         <div>{children}</div>
-        <CookieBanner />
       </div>
     </>
   );
