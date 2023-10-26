f31f12819fc82b7c56da34be325745d227486f53
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-25 19:10:10 -0500

    fixed project links mobile responsiveness

--- src/components/projects/ProjectModal.js
+++ src/components/projects/ProjectModal.js
@@ -52,7 +52,7 @@ const ProjectModal = (props) => {
           <h2 className="my-1 mb-2 text-center text-2xl font-bold underline sm:text-3xl">
             {props.project.name}
           </h2>
-          <div className="mx-auto mb-3 flex justify-around pt-4 sm:justify-evenly sm:pt-1">
+          <div className="mx-5 mb-3 flex justify-between pt-4 sm:mx-20 sm:justify-evenly sm:pt-1">
             {projectLinks.map((links) => {
               return (
                 <IconLinks
