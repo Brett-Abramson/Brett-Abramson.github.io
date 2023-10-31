8403408eddffc3548b183bbfe002e0452379563d
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-31 15:46:54 -0500

    changed a to button in ContactForm and added css spinner when loading state is True

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,33 +8,38 @@ const ContactForm = () => {
     message: "",
   });
   const [isEmailSent, setIsEmailSent] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
     setIsLoading(true);
+    try {
+      const response = await fetch("/api/sendEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
-    const response = await fetch("/api/sendEmail", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data.message);
 
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data.message);
-
-      setIsEmailSent(true);
-      setIsLoading(false);
-      resetFormData();
-    } else {
-      console.error("Error sending email");
-      setIsLoading(false);
+        setIsEmailSent(true);
+        setIsLoading(false);
+        resetFormData();
+      } else {
+        console.error("Error sending email");
+        setIsLoading(false);
+        setIsError(true);
+      }
+    } catch (error) {
+      console.error("Error:", error);
       setIsError(true);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -55,35 +60,36 @@ const ContactForm = () => {
       {isEmailSent ? (
         <div className="flex flex-col items-center">
           <p>Email successfully Sent!</p>
-          <a
+          <button
             href="#"
             onClick={() => {
               setIsEmailSent(false);
-              setIsLoading(false);
+              // setIsLoading(false);
             }}
           >
             Send another message?
-          </a>
+          </button>
         </div>
       ) : (
         <>
           {isLoading ? (
-            <p>Sending... Please wait.</p> // lets use a spinner here
+            <div className="flex flex-col items-center">
+              <p>Sending... Please wait.</p>
+              <div className="h-12 w-12 animate-spin rounded-full border-2 border-t-white border-opacity-30" />
+            </div>
           ) : (
             <>
               {isError ? (
-                <>
-                  {" "}
-                  <p>Error sending Email</p>
-                  <a
-                    href="#"
+                <div className="flex flex-col items-center">
+                  <p>Error Sending Email</p>
+                  <button
                     onClick={() => {
                       setIsError(false);
                     }}
                   >
-                    Try again.
-                  </a>
-                </>
+                    Please Try Again.
+                  </button>
+                </div>
               ) : (
                 <form onSubmit={handleSubmit} className="flex flex-col">
                   <label htmlFor="name">Name:</label>
