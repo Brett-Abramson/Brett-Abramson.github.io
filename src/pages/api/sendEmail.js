36b0465c789bd419cd81af889985b87d2ca224eb
Brett Abramson <brett.m.abramson@gmail.com>
2023-11-23 10:36:32 -0800

    added validation to email

--- src/pages/api/sendEmail.js
+++ src/pages/api/sendEmail.js
@@ -1,13 +1,41 @@
 import nodemailer from "nodemailer";
+import { validationResult, body } from "express-validator";
+
+const validateContactForm = [
+  body("name")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Name is Required"),
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Invalid email address.")
+    .normalizeEmail(),
+  body("message")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Message is required."),
+];
 
 export default async (req, res) => {
   const myEmail = process.env.MY_EMAIL;
   const portfolioEmail = process.env.PORTFOLIO_EMAIL;
   const portfolioPassword = process.env.PORTFOLIO_PASSWORD;
+
+  await Promise.all(
+    validateContactForm.map((validation) => validation.run(req))
+  );
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   try {
     if (req.method === "POST") {
       const { name, email, message } = req.body;
-
+      const websiteURL = "https://brett-abramson.vercel.app/";
+      const messageWithFooter = `${message}\n\nThis message was sent from ${websiteURL}`;
       // A Transporter using my email service credentials
       const transporter = nodemailer.createTransport({
         service: "Gmail",
@@ -22,7 +50,7 @@ export default async (req, res) => {
         from: email,
         to: myEmail,
         subject: `New Contact from ${name}`,
-        text: message,
+        text: messageWithFooter,
       };
 
       // send email
